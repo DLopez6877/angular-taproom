01f3ff6edf721c88cb35c7a24675361a57a9dfc2
Joe Parsley and Rose Sponseller <joe@>
2017-07-03 14:27:08 -0700

    Add Bootstrap

--- app/app.component.ts
+++ app/app.component.ts
@@ -33,7 +33,7 @@ export class AppComponent {
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
   tasks: Task[] = [
-    new Task('Finish weekend Angular homework for Epicodus course', 3),
+    new Task('HEY! Finish weekend Angular homework for Epicodus course', 3),
     new Task('Begin brainstorming possible JavaScript group projects', 2),
     new Task('Add README file to last few Angular repos on GitHub', 2)  ];
   selectedTask: Task = this.tasks[0];
